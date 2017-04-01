041d43bc9e8484acb15f6d19620d4f2e16615e9e
zrem123 <kak@armadilloaerospace.com>
2017-04-01 16:46:00 -0500

    GUI

--- src/newGame/MainGame.java
+++ src/newGame/MainGame.java
@@ -1,15 +1,21 @@
 package newGame;
 
+import sz.csi.ConsoleSystemInterface;
+import sz.csi.wswing.WSwingConsoleInterface;
+
 public class MainGame {
+	public static ConsoleSystemInterface csi;
 
     public static void main(String[] args) {
+        new MainGame();
         while(true) {
-            new MainGame();
         }
     }
 
     private MainGame() {
-
+		csi = new WSwingConsoleInterface();
+		csi.print(1, 1, "Why hi there! My name is 102313division by 0 error system(out) cancel~!#@!@@!");
+		
     }
 
 }
