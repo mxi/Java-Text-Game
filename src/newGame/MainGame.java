a89775478d4d355aaad39060648bbdc0c3d4b1ae
Maxim Kasyanenko <mkasyane@gmail.com>
2017-04-05 17:11:29 -0500

    Reverted to libjcsi library instead of JFX

--- src/newGame/MainGame.java
+++ src/newGame/MainGame.java
@@ -1,87 +1,19 @@
 package newGame;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.stage.Stage;
+import sz.csi.ConsoleSystemInterface;
+import sz.csi.wswing.WSwingConsoleInterface;
 
-public class MainGame extends Application {
+public class MainGame {
 
-    public static String CONFIG_DIR = "src/newGame/Configs/";
-    public static MainGame screen;
-    public static double aspectRatio = 4.0 / 3.0; // 4:3 aspect ratio
-    public static int widthOfTiles = 256;
-    public static int heightOfTiles = (int) (widthOfTiles / aspectRatio); // if 4:3 then height = 192
-    public static int tileWidth = 5;
-    public static int tileHeight = 5;
-    public static int width = widthOfTiles * tileWidth; // 1280
-    public static int height = heightOfTiles * tileHeight; // 960
-
-    private Stage stage;
-    private Scene scene;
-    private Pane layout;
-    private Canvas canvas;
-    private GraphicsContext graphics;
-
-    @Override
-    public void start(Stage istage) throws Exception {
-        screen = this;
-
-        stage = istage;
-        stage.setTitle("Santiago Zapata");
-
-        layout = new Pane();
-
-        canvas = new Canvas(width, height);
-        canvas.setLayoutX(0);
-        canvas.setLayoutY(0);
-
-        graphics = canvas.getGraphicsContext2D();
-        graphics.setFill(Color.BLACK);
-        graphics.fillRect(0, 0, width, height);
-
-        layout.getChildren().add(canvas);
-
-        scene = new Scene(layout, width, height);
-
-        stage.setScene(scene);
-        stage.sizeToScene();
-        stage.centerOnScreen();
-        stage.setResizable(false);
-        stage.show();
-    }
-
-    public Stage getStage() {
-        return stage;
-    }
-
-    public Scene getScene() {
-        return scene;
-    }
-
-    public Pane getLayout() {
-        return layout;
-    }
-
-    public Canvas getCanvas() {
-        return canvas;
-    }
-
-    public GraphicsContext getGraphics() {
-        return graphics;
-    }
-
-    private void setHandlers() {
-        // TODO: Add controls for the game later on.
-        canvas.setOnKeyTyped(key -> {});
-    }
-
-    // -- Main Method
+    public static ConsoleSystemInterface csi;
 
     public static void main(String[] args) {
-        launch(args);
+        new MainGame();
     }
+
+    private MainGame() {
+        csi = new WSwingConsoleInterface();
+        csi.print(1, 1, "Reverted back to CSI");
+    }
+
 }
