8ad6c85a715ec3222893fee023681c6e89185d8b
zrem123 <kak@armadilloaerospace.com>
2017-04-08 15:04:05 -0500

    not much

--- src/newGame/MainGame.java
+++ src/newGame/MainGame.java
@@ -8,12 +8,19 @@ public class MainGame {
     public static ConsoleSystemInterface csi;
 
     public static void main(String[] args) {
-        new MainGame();
+    	for(;;)
+    	{
+    		new MainGame();
+    	}
     }
 
     private MainGame() {
         csi = new WSwingConsoleInterface();
         csi.print(1, 1, "Reverted back to CSI");
+        int input = csi.inkey().code;
+        csi.print(1, 2, "You pressed " + input);
+        csi.refresh();
+        csi.waitKey(10);
     }
 
 }
