0cf4783c3d60ef062a7abe76093419d322f11883
ryan@gmail.com <ryan@gmail.com>
2017-07-15 15:57:08 -0500

    levelUp wrk

--- src/newGame/LevelUp/LevelUp.java
+++ src/newGame/LevelUp/LevelUp.java
@@ -1,11 +1,27 @@
 package newGame.LevelUp;
 
+import newGame.Entities.*;
+import newGame.Entities.Character;
+
 public class LevelUp {
 	
-	
-	public int levelUpAmount(Character c)
+	public boolean canLevelUp(Character c)
 	{
-		// return c.getexp + 1;
-		return 0;
+		return (c.getExp() > c.getExpUntilLevelUp());
+	}
+	
+	public int newExpUntilLevelUp(Character c)
+	{
+		return (int) (Math.pow(1.05, c.getLevel()) * 100);
+	}
+	
+	public void levelUp(Character c)
+	{
+		for(; c.getExp() >= c.getExpUntilLevelUp();)
+		{
+			c.setLevel(c.getLevel() + 1);
+			c.setExpUntilLevelUp(newExpUntilLevelUp(c));
+			c.setMaxHealth(c.getMaxHealth() + 5);
+		}
 	}
 }
